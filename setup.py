6b011da7c16f16956ce6d05d4bbc8882a085ef9d
Jan Helbling <jan.helbling@gmail.com>
2014-04-02 21:46:27 +0200

    Updated url (hostname, i have owned a domain ;))

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='py9kw',
       description='Captcharesolvingservice 9kw.eu - API',
       author='Jan Helbling',
       author_email='jan.helbling@gmail.com',
-      url='http://jan-helbling.no-ip.biz/',
+      url='http://www.jan-helbling.ch/index.php/projekte/17-python3-py9kw-eine-api-fuer-den-captcha-solving-service-von-9kw-eu',
       package_dir = {'py9kw' : 'lib'},
       packages=['py9kw'],
 )
