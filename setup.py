9fcbfbf97c15c81662be0da3621b5808758164ee
Jan Helbling <jan.helbling@gmail.com>
2018-06-11 23:58:54 +0200

    changed email-address

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='python3-py9kw',
       version='1.0',
       description='Captcharesolvingservice 9kw.eu - API',
       author='Jan Helbling',
-      author_email='jan.helbling@gmail.com',
+      author_email='jan.helbling@mailbox.org',
       url='https://github.com/JanHelbling/py9kw',
       py_modules=['py9kw']
 )
